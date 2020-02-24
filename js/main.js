2ac7ad5162a1a1a51baf471c02c80c882a2414f1
Alexey Klimuk <kazuss90@gmail.com>
2020-02-24 17:56:46 +0500

    сверстал раздел блог

--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
 $(document).ready(function () {
-   /* Портфолио */
-   $('.button[filter]').click(function () {
-      if ($(this).attr('val') == 'off') {
-         $('.button[filter]').attr('val', 'off').removeClass('focused');
-         $(this).attr('val', 'on').addClass('focused');
-         $('.filter > div').hide(300);
-         $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
-         if ($(this).attr('filter') == 'all') {
-            $('.button[filter]').attr('val', 'off').removeClass('focused');
-            $(this).attr('val', 'on').addClass('focused');
-            $('.filter > div').show(300);
-         }
-      }
-   })
+	/* Портфолио */
+	$('.button[filter]').click(function () {
+		if ($(this).attr('val') == 'off') {
+			$('.button[filter]').attr('val', 'off').removeClass('focused');
+			$(this).attr('val', 'on').addClass('focused');
+			$('.filter > div').hide(300);
+			$('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
+			if ($(this).attr('filter') == 'all') {
+				$('.button[filter]').attr('val', 'off').removeClass('focused');
+				$(this).attr('val', 'on').addClass('focused');
+				$('.filter > div').show(300);
+			}
+		}
+	})
+// Slider
+	$('.multiple-items').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		arrows: false,
+		dots: true,
+		dotsClass: 'dots-style',
+		responsive: [{
+			breakpoint: 1025,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		}]
+	})
+
 })
